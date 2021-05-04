51bb9def07372c8baf85d1aa3ab974ace5f12cba
BuGalter <wardoc78@gmail.com>
2021-05-04 19:58:43 +0300

    Added file refs-on-element.js for storage refs on elements

---
+++ src/refs-on-element.js
@@ -0,0 +1,10 @@
+export const refs = [
+    {
+        "title": "Ссылка на UI kit Colors and Type",
+        "ref": "#"
+    },
+    {
+        "title": "Ссылка на UI kit Colors and Type",
+        "ref": "#"
+    }
+];
