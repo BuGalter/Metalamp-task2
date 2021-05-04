8b9938b15a3b5d37e79ad428bfe145c0884ec90e
BuGalter <wardoc78@gmail.com>
2021-05-04 20:00:06 +0300

    Added to index page dynamic formation of the list of links

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
+import {refs} from './refs-on-element';
 import './index.scss';
 
-/*
-document.addEventListener("DOMContentLoaded", () => { 
-    alert('Webpack - рулит!!!');
-});
-*/
+//Ждем загрузки дом дерева, событие подходит для добавления узлов
+document.addEventListener("DOMContentLoaded", addListRefs());
+
+/**
+  * Формируем список ссылок на сверстаные страницы проекта
+  * @name addListRefs()
+  * @private Находим блок, создаем список, добавляем в дерево, создаем элемент списка,
+  * @private добавляем элемент в список, добавляем ссылку.
+  * @private Импортируем массив объектов, которые содержат данные для формирования ссылок.
+  */
+function addListRefs() {
+    let listNode = document.body.querySelector('.list-refs');
+    let list = document.createElement('ul');
+    listNode.append(list);
+    list.classList.add('list-refs__list');
+    refs.forEach((refAttributes) => {
+        let listItem = document.createElement('li');
+        list.append(listItem);
+        let ref = document.createElement('a');
+        ref.href = refAttributes.ref;
+        ref.text = refAttributes.title;
+        listItem.append(ref);
+    });
+};
