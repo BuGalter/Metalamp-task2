3e23cbaf8f418e7670096a99aad6acdf84c8cd8e
BuGalter <wardoc78@gmail.com>
2021-05-05 20:43:27 +0300

    Added on index page links to all project pages in refs-on-element

--- src/refs-on-element.js
+++ src/refs-on-element.js
@@ -1,10 +1,42 @@
 export const refs = [
     {
-        "title": "Ссылка на UI kit Colors and Type",
+        "title": "Ссылка на UI kit",
         "ref": "#"
     },
     {
-        "title": "Ссылка на UI kit Colors and Type",
+        "title": "Ссылка на Landing page",
+        "ref": "#"
+    },
+    {
+        "title": "Ссылка на Landing page/Dates Dropdown",
+        "ref": "#"
+    },
+    {
+        "title": "Ссылка на Landing page/Guests Dropdown",
+        "ref": "#"
+    },
+    {
+        "title": "Ссылка на Search room/Filter",
+        "ref": "#"
+    },
+    {
+        "title": "Ссылка на Search room/Filter/Preferences Expandable Checkbox list",
+        "ref": "#"
+    },
+    {
+        "title": "Ссылка на Search room/Filter/Preferences Dropdown",
+        "ref": "#"
+    },
+    {
+        "title": "Ссылка на Room details",
+        "ref": "#"
+    },
+    {
+        "title": "Ссылка на Sign in",
+        "ref": "#"
+    },
+    {
+        "title": "Ссылка на Registration",
         "ref": "#"
     }
 ];
